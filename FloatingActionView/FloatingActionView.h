bcd1c1ea4a2c660b8c6166a12ca7adef073672ca
Hidemune Takahashi <hidemune.takahashi@outlook.com>
2018-12-21 23:42:44 +0900

    Initial Commit

---
+++ FloatingActionView/FloatingActionView.h
@@ -0,0 +1,19 @@
+//
+//  FloatingActionView.h
+//  FloatingActionView
+//
+//  Created by hidemune on 12/21/18.
+//  Copyright © 2018 Hidemune Takahashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FloatingActionView.
+FOUNDATION_EXPORT double FloatingActionViewVersionNumber;
+
+//! Project version string for FloatingActionView.
+FOUNDATION_EXPORT const unsigned char FloatingActionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatingActionView/PublicHeader.h>
+
+
